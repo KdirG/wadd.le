c28663ee02e78a4959a6a9d167f56153b4912633
KdirG <22220030102@mersin.edu.tr>
2026-03-01 15:59:35 +0300

    second commit

---
+++ src/main/java/com/project/waddle/WaddleApplication.java
@@ -0,0 +1,13 @@
+package com.project.waddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaddleApplication.class, args);
+	}
+
+}
