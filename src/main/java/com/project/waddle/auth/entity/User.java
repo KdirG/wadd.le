95f750d61475f464061d74d99be1bec990008518
KdirG <22220030102@mersin.edu.tr>
2026-03-01 16:51:25 +0300

    third commit

---
+++ src/main/java/com/project/waddle/auth/entity/User.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project.waddle.auth.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id; // Entity için Id de gerekecek
+import java.io.Serializable;
+import java.util.UUID;
+
+/**
+ *
+ * @author kadir
+ */
+@Entity
+public class User {
+    
+}
